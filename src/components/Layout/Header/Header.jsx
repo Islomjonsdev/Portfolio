0a7f285856f7887d1b29eb594d706f530132a669
Islomjons <mukhammadalievislomjon@gmail.com>
2024-01-15 11:30:49 +0500

    fixed

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./Header.scss"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header'>
+        <div className='container'>
+            <div className='header__container'>
+                <Link className='header__home__link' to="/">
+                  <span>1</span>
+                </Link>
+
+                <ul className='header__list'>
+                    <li>
+                        <Link to="/home">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/blog">Blog</Link>
+                    </li>
+                    <li>
+                        <Link to="/resume">Resume</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </header>
   )
 }
 
--- src/pages/RouterPage/RouterPage.jsx
+++ src/pages/RouterPage/RouterPage.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Layout from '../../components/Layout/Layout'
+import About from '../About/About'
 import Auth from '../Auth/Auth'
+import Blog from '../Blog/Blog'
+import Home from '../Home/Home'
+import Resume from '../Resume/Resume'
 
 const RouterPage = () => {
   return (
     <div>
         <Routes>
-            <Route path='/' element={<Auth />}/>
-            <Route path='/main' element={<Layout />}>
-
+            {/* <Route path='/' element={<Auth />}/> */}
+            <Route path='/' element={<Layout />}>
+                <Route path='/home' element={<Home />}/>
+                <Route path='/about' element={<About />}/>
+                <Route path='/blog' element={<Blog />}/>
+                <Route path='/resume' element={<Resume />}/>
             </Route>
         </Routes>
     </div>
